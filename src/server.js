eb62e16704f587ee7e7b7a2adf156965b451fa9a
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-09-21 21:12:23 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { connection } = require('./database/connection');
+const cors = require('cors');
+
+class Server {
+    constructor ( server = express())
+    {
+        this.middlewares(server)
+        this.database()
+        this.initializeServer(server)
+    }
+    async middlewares(app) {
+        app.use(cors())
+        app.use(express.json())
+    }
+    async database() {
+        try {
+            await connection.authenticate();
+            console.log('Conexão bem sucedida!')
+        } catch (error) {
+            console.error('Não foi possível se conectar', error)
+        }
+    }
+    async initializeServer(app) {
+        const PORT = 3333
+        app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`))
+    }
+    
+};
+
+module.exports = { Server }
